25e8afb1609acbc6134e84e172db68f94880b84e
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-09 12:39:41 -0700

    task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -15,15 +15,15 @@ n = rand() - RAND_MAX / 2;
 e = n % 10;
 if (e > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n,e);
+printf("Last digit of %d is %d and is greater than 5\n", n, e);
 }
 else if (e == 0)
 {
-printf("Last digit of %d is%d and is 0\n", n,e);
+printf("Last digit of %d is %d and is 0\n", n, e);
 }
 else if ((e < 6) && (e != 0))
 {
-printf("Last digit of %d is %d and is less than 6 and not 0\n", n,e);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, e);
 }
 return (0);
 }
