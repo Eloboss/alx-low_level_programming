f86fef2fde7d1dd6968154525d3a6afd1ed3abec
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-09 13:55:55 -0700

    task 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- Entry point
+ *
+ * Return: Sign of numbers
+ */
+int main(void)
+{
+int e;
+e = 0;
+while (e < 10)
+{
+printf("%d", e++);
+}
+return (0);
+}
