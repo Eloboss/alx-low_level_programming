ebb969e3dcdac6cfc3a82602c10622559d6362de
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-09 14:18:43 -0700

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- Entry point
+ *
+ * Return: Sign of numbers
+ */
+int main(void)
+{
+int i; 
+i = 0;
+while (i < 10)
+{ 
+putchar(i);
+}
+putchar('\n')
+return (0);
+}
