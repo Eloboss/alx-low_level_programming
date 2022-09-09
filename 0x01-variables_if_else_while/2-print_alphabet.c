71b74d22e89abbe8f8a6fc6d499a1543cd7ad7a2
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-09 12:48:52 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- Entry point
+ *
+ * Return: Sign of numbers
+ */
+int main(void)
+{
+char e;
+for (e = 'a'; e <= 'z'; e++)
+{
+putchar(e);
+}
+return (0);
+}
