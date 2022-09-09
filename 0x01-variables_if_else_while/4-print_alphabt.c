7cd1709f37b066ea21946b87c51171bae3aeb063
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-09 16:17:41 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- Entry point
+ *
+ * Return: Sign of numbers
+ */
+int main(void)
+{
+char e;
+for (e = 'a'; e <= 'z'; e++)
+{
+if (e = 'q' || e = 'e')
+{
+continue;
+putchar(e);
+}
+}
+putchar('\n');
+return (0);
+}
