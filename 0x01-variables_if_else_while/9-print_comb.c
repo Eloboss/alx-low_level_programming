1a1f40d50abfd9b23c058e3ce363af515d28e0ed
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-09 15:00:11 -0700

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- Entry point
+ *
+ * Return: Sign of numbers
+ */
+int main(void)
+{
+int e;
+for (e = 0; e <= 9; e++)
+{
+putchar(e);
+}
+putchar(' ');
+putchar(',');
+return (0);
+}
