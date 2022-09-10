ab47385f0a6997ce68952ef54d0c23fe82fa54e8
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-09 17:11:30 -0700

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+/**
+ * main- Entry point
+ *
+ * Return: Sign of numbers
+ */
+int main(void)
+{
+int e;
+int p;
+for (e = 48; e <= 56; e++)
+{
+for (p = 49; p <= 57; p++)
+{
+putchar(ep);
+if ((e != 56) && (p != 57))
+{
+putchar(',');
+putchar(' ');
+}
+}
+}
+putchar('\n');
+return (0);
+}
