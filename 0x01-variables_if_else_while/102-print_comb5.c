6cb9cc8297de5151dd0b1e54c5815c5149f93586
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-09 19:54:29 -0700

    task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -13,12 +13,12 @@ int main(void)
 {
 int i;
 int j;
-int k;
 for (i = 0; i < 100; i++)
 {
 for (j = 0; j < 100; j++)
 {
 if (i < j)
+{
 putchar((i / 10) + 48);
 putchar((i % 10) + 48);
 putchar(' ');
@@ -28,7 +28,6 @@ if (i != 98 || j != 99)
 {
 putchar(',');
 putchar(' ');
-}
 }
 }
 }
