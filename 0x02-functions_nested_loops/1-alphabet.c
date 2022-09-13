9b992b589ded1464613ba9dd48b302de18f17835
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-13 11:39:19 -0700

    task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main- print putchar
+ *
+ *
+ * Return: Always return 0 on success
+ */
+void print_alphabet(void);
+{
+char e;
+for (e = 'a'; e <= 'z'; e++)
+{
+_putchar(e);
+}
+_putchar('\n');
+return (0);
+}
