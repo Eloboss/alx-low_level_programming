58e5281006dab90745e93b155236d0d04a777e1b
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-13 13:37:28 -0700

    task 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_sign- Print sign of numbers
+ * @n: Number to b evaluated
+ *
+ * Return: Always 0 on success
+ */
+int print_sign(int n)
+{
+if (n > 0)
+{
+_putchar('+');
+_putchar(',');
+_putchar(' ');
+return (1);
+else if (n ==0)
+{
+_putchar(48);
+_putchar(',');
+_putchar(' ');
+return (0);
+else if (n < 0)
+_putchar(45);
+_putchar(',');
+_putchar(' ');
+return (-1);
