8284e990778b086533a806def67d25f96491509d
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-13 12:28:57 -0700

    task 3

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * -islower- Detects lowercase letters
+ *
+ *
+ *  Return: Always 0 on success
+ */
+int _islower(int c)
+if (c > 96 && c < 113)
+{
+_putchar(49);
+else
+{
+_putchar(48);
+}
+}
