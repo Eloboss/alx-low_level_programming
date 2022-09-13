45ce6d46dbf658a84c1aca1593dd9545db6e8ba8
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-13 16:39:23 -0700

    task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98- print numbers to 98
+ * @n: Number to b evaluated
+ *
+ */
+void print_to_98(int n)
+{
+while (n < 98)
+{
+printf("%d, ", n);
+n++;
+printf("\n");
+}
+while (n > 98)
+{
+printf("%d, ", n);
+n--;
+printf("\n);
+}
+while (n = 98)
+{
+printf("%d", n);
+printf("\n");
+}
+}
