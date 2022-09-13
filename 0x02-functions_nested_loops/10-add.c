d840fc65e8c890b810fb0becdb4642cc591cbeb4
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-13 15:57:35 -0700

    task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * add- Add two numbers
+ * @a: Number to,b evaluated
+ * @b: Number to b evaluated
+ *
+ * Return: Always 0 on success
+ */
+int add (int a, int b)
+{
+return a + b;
+}
