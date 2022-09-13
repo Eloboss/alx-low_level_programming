eb0de30b6574fe6927ba8f6bb1ebd8141e7dc804
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-13 15:47:29 -0700

    task 8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * jack_bauer- Prints a clock
+ * 
+ * 
+ */
+void jack_bauer(void)
+{
+for (i = 0; i < 24; i++)
+{
+for (p = 0; p < 60; p++)
+{
+_putchar(i / 10 + '0');
+_putchar(i % 10 + '0');
+_putchar(':');
+_putchar(p / 10 + '0');
+_putchar(p % 10 + '0');
+_putchar('\n');
+}
+}
+}
