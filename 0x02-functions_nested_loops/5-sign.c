3ab51f7326fbc5e7fc71212d1d3bf5c56a34a6e3
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-13 13:43:48 -0700

    task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -13,14 +13,19 @@ _putchar('+');
 _putchar(',');
 _putchar(' ');
 return (1);
+}
 else if (n ==0)
 {
 _putchar(48);
 _putchar(',');
 _putchar(' ');
 return (0);
+}
 else if (n < 0)
+{
 _putchar(45);
 _putchar(',');
 _putchar(' ');
 return (-1);
+}
+}
