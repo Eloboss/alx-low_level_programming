769b7615ed0460fb87777bfd49fc10a9d4c93bd4
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-13 18:16:59 -0700

    task 14

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -11,17 +11,18 @@ long int i, j, k, l;
 j = 1;
 k = 2;
 for (i = 1; i <= 50; ++i)
-	{
-		if (j != 20365011074)
-		{
-			printf("%ld, ", j);
-		} else
-		{
-			printf("%ld\n", j);
-		}
-		l = j + k;
-		j = k;
-		k = l;
-	}
-        return (0);
+{
+if (j != 20365011074)
+{
+printf("%ld, ", j);
+}
+else
+{
+printf("%ld\n", j);
+}
+l = j + k;
+j = k;
+k = l;
+}
+return (0);
 }
