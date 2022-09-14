5548d91ddff590cfe3f03d5c9eab8fa1bfb2bc32
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-13 17:34:03 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -11,14 +11,17 @@ int print_last_digit(int n)
 if (n >= 0 && n <= 9)
 {
 return (n);
+return (n);
 }
-else if(n < 9)
+else if (n > 9)
 {
+return (n % 10);
 return (n % 10);
 }
 else
 {
 n *= -1;
-return(n % 10);
+return (n % 10);
+return (n % 10);
 }
 }
