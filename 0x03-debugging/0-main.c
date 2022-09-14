073a7ff0337b54e4f5be1279b98037021122ef1b
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-14 05:51:02 -0700

    task 0

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,13 +1,17 @@
 #include "main.h"
+
 /**
  * main- Prints positive or negative numbers
  *
  * Return: Always 0 on success
  */
+
 int main(void)
 {
 int i;
+
 i = 98;
 positive_or_negative(i);
+
 return (0);
 }
