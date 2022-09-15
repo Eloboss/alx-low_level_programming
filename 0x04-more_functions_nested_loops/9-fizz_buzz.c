743c794c62d46580d65dff1cb7df474b1bd89be3
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-15 16:46:52 -0700

    task 9

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -9,22 +9,18 @@
 
 int main(void)
 {
-	int n;
-	for (n = 1; n < 100; n++)
-	{
-		if (n % 3 == 0)
-		{
-			printf("Fizz");
-		else if (n % 5 == 0)
-		{
-			printf("Buzz");
-		else if (n % 3 == 0 && n % 5 == 0);
-		{
-			printf("FizzBuzz");
-		else
-		{
-			printf ("%d ", n);
-	}}}}}
-	printf("Buzz\n");
-	return (0);
+int n;
+for (n = 1; n < 100; n++)
+{
+if (n % 3 == 0)
+printf("Fizz");
+else if (n % 5 == 0)
+printf("Buzz");
+else if (n % 3 == 0 && n % 5 == 0)
+printf("FizzBuzz");
+else
+printf("%d ", n);
+}
+printf("Buzz\n");
+return (0);
 }
