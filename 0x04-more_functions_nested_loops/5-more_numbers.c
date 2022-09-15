51e1bc6ff7e149537114756e4589203f425fd575
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-15 01:59:27 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers- print 0 to 14 ten times
+ *
+ */
+void more_numbers(void)
+{
+int e;
+int p;
+for (e >= 0; e <= 9; e++)
+{
+for (p >= 48; p<= 72; p++)
+{
+_putchar(p);
+}
+_putchar('\n)
+return (0);
+}
+}
