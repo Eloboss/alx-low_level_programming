14a7b81f6c17f9e5b730206915d942aef2be8ee7
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-15 15:45:28 -0700

    task 7

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_diagonal- Print diagonal
+ */
+
+void print_diagonal(int n)
+{
+int e = 1;
+if (n > 0)
+{
+while (e <= n)
+{
+_putchar(' ');
+_putchar(92);
+e++;
+}
+_putchar('\n');
+}
+}
