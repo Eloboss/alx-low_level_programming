e7e23f48709f59160f7e5274687ccee981a2c95a
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-15 21:10:50 -0700

    task 10

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,18 +7,25 @@
 
 void print_triangle(int size)
 {
-	int i;
-	int j;
+
+	int i = 0, j, n = size - 1;
 
 	if (size > 0)
 	{
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
+		for (; i < size; i++)
 		{
-			_putchar(' ');
-			_putchar(35);
+			for (j = 0; j < size; j++)
+			{
+				if (j < n)
+					_putchar(' ');
+				else
+					_putchar('#');
+			}
+			n--;
+			_putchar('\n');
 		}
+	}
+	else
 		_putchar('\n');
-	}}
 }
+
