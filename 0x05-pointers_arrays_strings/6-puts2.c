e8a530f5083599fafd566e0c6798088c4449b161
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-19 18:50:35 -0700

    tadk 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * puts2- print half a character
+ * @str: String to be evaluated
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+	for ( ; s[i] != '\0'; i += 2)
+		_putchar(s[i]);
+	_putchar('\n');
+}
