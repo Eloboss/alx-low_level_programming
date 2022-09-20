762298c1ccbef8f025cd91b4af52a7ab80ec1963
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-19 17:24:55 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _puts- Prints strings 
+ * @str: String to be used
+ */
+
+void _puts(char *str)
+{
+	long p;
+
+	p = 0;
+
+	for ( ; str[p] != '\0'; p++)
+		_puts(str[p]);
+}
