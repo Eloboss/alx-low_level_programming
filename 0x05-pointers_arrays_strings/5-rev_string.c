77b2fc390b48efbdde8eda40ab04dd4e1532e545
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-19 18:29:44 -0700

    task 5

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -12,9 +12,11 @@ void rev_string(char *s)
 
 	for ( ; s[i] != '\0'; )
 	{
+		_putchar(s[i]);
 		i++;
 	}
-	for (j = i -1; j >= 0; j--)
+	_putchar('\n');
+	for (j = i - 1; j >= 0; j--)
 		_putchar(s[j]);
 	_putchar('\n');
 }
