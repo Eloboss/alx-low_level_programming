6f7bc4b0e90a25a302a6fd60bb6a6e1655a2e099
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-19 18:21:20 -0700

    task 5

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * rev_string- Reverses a string
+ * @s: String to be evaluated
+ */
+
+void rev_string(char *s)
+{
+	long i = 0;
+	long j;
+
+	for ( ; s[i] != '\0'; )
+	{
+		i++;
+	}
+	for (j = i -1; j >= 0; j--)
+		_putchar(s[j]);
+	_putchar('\n');
+}
