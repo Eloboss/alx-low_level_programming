1025a96c738fa77c2c96bc07fb7cb39455c5c766
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-22 17:17:27 -0700

    task 1

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strncat- Catenates n bytes
+ * @dest: String to b joined
+ * @src: String to b joined
+ * @n: n bytes
+ *
+ *Return: Always 0
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	
