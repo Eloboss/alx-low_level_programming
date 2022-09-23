b6664fc3e13fc329c0c52f0ada11eead02434ed2
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-22 18:46:29 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,4 +9,18 @@
 
 char *string_toupper(char *e)
 {
+		int length;
+
+	length = 0;
+
+	while (e[length] != '\0')
+	{
+		if (e[length] >= 97 && e[length] <= 122)
+		{
+			e[length] = e[length] - 32;
+		}
+		length++;
+	}
+	return (e);
+}
 
