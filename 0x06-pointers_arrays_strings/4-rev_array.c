fe544811e2dfaf4cfba546cdfa27e30759f54916
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-22 18:40:15 -0700

    task 4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,3 +9,15 @@
 void reverse_array(int *a, int n)
 {
 
+	int temp, counter;
+
+	n = n - 1;
+	counter = 0;
+	while (counter <= n)
+	{
+		temp = a[counter];
+		a[counter++] = a[n];
+		a[n--] = temp;
+	}
+}
+
