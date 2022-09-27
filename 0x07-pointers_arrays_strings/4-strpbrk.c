eb828fd9d81b9c11a728b18165cd09cb99dffc73
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-26 18:21:30 -0700

    task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strpbrk: Searches a string
+ * @s: String to,b used
+ * @accept: Character to b matched
+ *
+ * Return: Always 0
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+		int x = 0, y;
+
+	while (s[x] != '\0')
+	{
+		for (y = 0; accept[y] != '\0'; y++)
+		{
+			if (s[x] == accept[y])
+			{
+				s = &s[x];
+				return (s);
+			}
+
+		}
+
+	x++;
+	}
+
+	return (NULL);
+}
