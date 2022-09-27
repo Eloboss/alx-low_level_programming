c29ffeb469162dd92cc1ca63e3c2a41f6d760f7e
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-26 18:52:20 -0700

    task 9

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * set_string- Sets d value of a pointer to a char
+ * @s: String to b used
+ * @to: String to b used
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
