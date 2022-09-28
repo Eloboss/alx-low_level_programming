7b3e1715ea2faf5355950fc431a4efa017393292
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-28 00:50:16 -0700

    task 8

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * wildcmp- Compares two strings
+ * @s1: String to b evaluated
+ * @s2: String to b evaluated
+ *
+ * Return: Always 0
+ */
+
+int wildcmp(char *s1, char *s2)
+{
