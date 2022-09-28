38a714c748c1322bf035e700e739d5f4e71d6722
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-28 00:32:06 -0700

    task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _pow_recursion- Finds d power of a number
+ * @x: Number to b used
+ * @y: Number to b used
+ *
+ * Return: Always 0
+ */
+
+int _pow_recursion(int x, int y)
+{
