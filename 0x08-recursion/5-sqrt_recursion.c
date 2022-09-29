8f889fffadb0596261e47cf372d10ff64624e604
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-28 21:02:39 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -28,7 +28,8 @@ int helper(int a, int b)
  */
 int _sqrt_recursion(int n)
 {
-        if (n == 0)
-                return (0);
-        return (helper(n, 1));
+
+	if (n == 0)
+		return (0);
+	return (helper(n, 1));
 }
