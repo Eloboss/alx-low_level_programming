a0c83d58edc644a582f7a4070fa244ee576b3272
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-28 20:20:04 -0700

    task 5

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,22 +1,6 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion- Finds d square of a number
- *
- * @n: Number to b used
- * 
- * Return: Always 0
- */
-
-int _sqrt_recursion(int n)
-{
-	int _sqrt_recursion(int n)
-{
-	if (n == 0)
-		return (0);
-	return (helper(n, 1));
-}
-/**
  * helper - Finds the square root of two numbers
  * @a: The number
  * @b: The number to test for the square root of @a
@@ -34,4 +18,21 @@ int helper(int a, int b)
 		return (helper(a, b + 1));
 
 	return (1);
+}
+
+/**
+ * _sqrt_recursion- Finds d square of a number
+ *
+ * @n: Number to b used
+ *
+ * Return: Always 0
+ */
+
+int _sqrt_recursion(int n)
+{
+        int _sqrt_recursion(int n)
+{
+        if (n == 0)
+                return (0);
+        return (helper(n, 1));
 }
