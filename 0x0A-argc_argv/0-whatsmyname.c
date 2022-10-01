f548981debaa6472c38c2e1e7df297e5bca8f51c
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-30 18:37:20 -0700

    task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- To print programe name
+ * @argc: Arguments count
+ * @argv: Argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	void(argc);
+	{
+		printf("%i\n", argv[0]);
+	}
+	return (0);
+}
+
