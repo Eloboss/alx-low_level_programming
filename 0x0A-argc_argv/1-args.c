2a968f9f9ad96fd91aacb8b9fe198c24d8486e1e
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-30 18:54:23 -0700

    task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main- count number of arguments
+ * @argc: Argument counts
+ * @argv: Argument vector
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	{
+		printf("%d\n", argc - 1)
+	}
+	return (0);
+}
