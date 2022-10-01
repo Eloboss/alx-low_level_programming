12684680ed1bf29ab5556e535ab2957b16c3f9d7
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-30 20:09:20 -0700

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,6 +13,7 @@
 int main(int argc, char *argv[])
 {
 	int i;
+	int j;
 	int sum = 0;
 
 	if (argc == 1)
@@ -21,11 +22,14 @@ int main(int argc, char *argv[])
 	}
 	for (i = 1; i < argc; i++)
 	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
 		if (!isdigit(argv[i])
 				{
 				printf("Error\n");
 				return (1);
 				}
+		}
 		sum += atoi(argv[i]);
 		printf("%d\n", sum);
 	}
