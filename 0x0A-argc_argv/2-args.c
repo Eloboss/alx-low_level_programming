b42dec5936975caeb66ba10967e73f032e89c346
Eloboss <elochukwunwaigbo@gmail.com>
2022-09-30 20:31:05 -0700

    task 2

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -10,19 +10,19 @@
  * Return: Always 0
  */
 
-int main(int argc, char *arg[v])
+int main(int argc, char *argv[])
 {
 	int i;
 
 	if (argc > 0)
 	{
-	
+
 		for (i = 0; i < argc; i++)
 		{
 
-		
+
 			printf("%s\n", argv[i]);
-	
+
 		}
 	}
 	return (0);
