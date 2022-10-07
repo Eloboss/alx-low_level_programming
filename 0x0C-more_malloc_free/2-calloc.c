920c1024ea1b323d806617deb56b826d3744b606
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-06 21:24:40 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -4,6 +4,24 @@
 #include <stddef.h>
 
 /**
+ * _memset - copy char
+ * @s: string
+ * @b: input
+ * @n: bytes
+ * Return: string
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+/**
  * *_calloc- Allocates memory
  * @nmemb: Number of elements
  * @size: Number of bytes
@@ -26,7 +44,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	{
 		return (NULL);
 	}
-	for (i = nmemb * size ; e = p; i; i--)
-		*p++ = 0;
+
+	_memset(p, 0, (nmemb * size));
 	return (e);
 }
