5543ebf6ee4019313ae2b873e0bb48fcb1cae801
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-10 08:28:53 -0700

    task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,31 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * print_dog- Prints a type of dog
+ * @d: Dog identifier
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d->name == NULL)
+	{
+		printf("Name: nil\n");
+	}
+	else
+		printf("Name: %s\n", d->name);
+	if (d->age == NULL)
+	{
+		printf("Age: nil\n");
+	}
+	else
+		printf("Age: %d\n", d->age);
+	if (d->owner == NULL)
+	{
+		printf("Owner: nil\n");
+	}
+	else
+		printf("Owner: %s\n", d->owner);
+	if (d == NULL)
+		printf("");
+}
