f33c5a60eb9238f871f21fd9c74d69279044bda6
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-10 08:44:50 -0700

    task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * new_dog- To print a new dog
+ * @name: Name of dog
+ * @age: Age of d dog
+ * @owner: Owner of d dog
+ * Return: Always 0
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *pim;
+	
+	pim = malloc(sizeof(char) * (strlen(name) + strlen(age) + 1);
+			if (p == NULL)
+			{
+			return (NULL);
+			}
+			return (p);
+}
