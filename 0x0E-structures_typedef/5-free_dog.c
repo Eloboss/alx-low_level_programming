3d2720224dc38371bc6bcf4562d4f339d3bd3223
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-10 09:16:37 -0700

    task 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stddef.h>
+#include <sttdlib.h>
+
+/**
+ * free_dog- Fress dogs
+ * @d: Dog identifier
+ */
+
+void free_dog(dog_t *d)
+{
+	if (d)
+	{
+		if (d->owner)
+		{
+			free(d->owner);
+		}
+		if (d->name)
+		{
+			free(d->name);
+		}
+		free(d);
+	}
+}
