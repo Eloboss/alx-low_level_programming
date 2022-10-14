09a1fac37241945cbac993321ed7a65d5eaee1c3
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-13 21:11:24 -0700

    task 1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -12,11 +12,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 
 	va_start(elo, n);
-
-
+	
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(elo, int);
+		printf("%d", va_arg(elo, int));
 		if (separator && i < n - 1)
 			printf("%s", separator);
 	}
