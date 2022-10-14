2ce47ee11d3f931b8c93af13e54e04a2ff767369
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-13 20:47:31 -0700

    task 2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings- Prints Strings
+ * @separator: Seperator to b used
+ * @n: Number of strings
+ *
+ * Return: void
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list elo;
+	unsigned int i;
+	chat *str;
+
+	va_start(elo, n);
+
+		for (i = 0; i < n; i++)
+	{
+		str = va_arg(elo, char *);
+
+		if (str)
+			printf("%s", str);
+		else
+			printf("(nil)");
+
+		if (i < n - 1)
+			if (separator)
+				printf("%s", separator);
+	}
+
+	printf("\n");
+	va_end(elo);
+}
