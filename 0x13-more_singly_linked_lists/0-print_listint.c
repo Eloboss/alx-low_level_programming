32ff434329b9308a545b7fa5d08d784f923da262
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-24 23:30:32 -0700

    task 0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint- Print all elements of a list
+ * @h: string to b used
+ * Return: Always 0
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	listint_t *list;
+	unsigned int count = 0;
+
+	for (h = list; h != NULL; h = h->next)
+	{
+		printf("%d\n", h->n);
+		count++;
+	}
+	printf("-> %lu elements\n", count);
+}
