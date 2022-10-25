49da902effa3b620abc241a267e0bf4e725b1d3a
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-25 00:33:50 -0700

    task 3

---
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -0,0 +1,36 @@
+#include "lists.h"
+
+/**
+ * add_nodeint_end- Add node at d end
+ * @head: end of d node
+ * @n: Value
+ * Return: Always 0
+ */
+
+listint_t *add_nodeint_end(listint_t **head, const int n)
+{
+	listint_t *boss;
+	listint_t *elo;
+
+	elo = malloc(sizeof(listint_t));
+	if (elo)
+	{
+		elo->n = n;
+		elo->next = NULL;
+		return (elo);
+	}
+	else
+		return (NULL);
+	boss = *head;
+
+	if (boss == NULL)
+	{
+		return (NULL);
+	}
+	else
+	for ( ; boss != NULL; boss = boss->next)
+	{
+	boss->next = elo;
+	return (boss);
+	}
+}
