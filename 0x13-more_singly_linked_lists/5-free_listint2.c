a3422b9cbca107200e8adbc67eb58483f19489f1
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-25 05:30:26 -0700

    task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+
+/**
+ * free_listint2- fress memory
+ * @head: Beginning of d string
+ */
+
+void free_listint2(listint_t **head)
+{
+	if (*head)
+	{
+		free(head->next);
+		free(head);
+	}
+	
+	if (*head == NULL)
+	{
+		return;
+	}
+
+	*head == NULL;
+}
