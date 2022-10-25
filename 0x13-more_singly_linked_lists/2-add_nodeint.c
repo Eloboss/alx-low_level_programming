10cdf8b7378d0e45859ba95e7a2b18c89ecbcb01
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-25 00:01:21 -0700

    task 2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * add_nodeint- Adds node ad d beginning
+ * @head: Beginning of d node
+ * @n: Value
+ * Return: Always 0
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *elo;
+
+	elo = malloc(sizeof(listint_t);
+			if (elo)
+			{
+			elo->n = n;
+			elo->next = *head;
+			elo = *head;
+			return (*head);
+			}
+			else
+			{
+			return (NULL);
+			}
+}
