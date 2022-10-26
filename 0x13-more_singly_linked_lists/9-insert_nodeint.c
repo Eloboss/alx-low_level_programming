2b408f345cd25ca87065e7c28f0a22d38fbd337a
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-25 21:48:26 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -12,8 +12,7 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *elo;
-	listint_t *boss;
-	unigned int i;
+	size_t int i;
 
 	elo = malloc(sizeof(listint_t));
 	if (!elo)
