ce2b4831c66f359a4ccc3d8902891e6aba5e77e3
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-25 21:23:34 -0700

    task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,11 @@ void free_listint2(listint_t **head)
 {
 	listint_t *elo;
 
-	while ((*head)->next)
+	while (*head)
 	{
 		elo = *head;
 		*head = (*head)->next;
-		free(*head);
+		free(elo);
 	}
 	*head == NULL;
 }
