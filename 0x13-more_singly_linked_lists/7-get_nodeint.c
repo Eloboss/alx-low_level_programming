32c4717b132ee2322714efc0bc683f243d065494
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-25 17:32:38 -0700

    task 7

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -14,15 +14,15 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	count = 0;
 
-	if (!head || count > index)
+	if (!head && count > index)
 	{
 		return (NULL);
 	}
 	else
 		if (count == index)
-			{
-				return (head->n);
-				count++;
-				head = head->next;
-			}
+		{
+			return (head->n);
+			count++;
+			head = head->next;
+		}
 }
