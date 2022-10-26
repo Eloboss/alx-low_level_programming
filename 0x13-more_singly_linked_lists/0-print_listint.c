d95c89679a02757f2f3476045329304834f22655
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-25 20:31:20 -0700

    task 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -9,13 +9,15 @@
 size_t print_listint(const listint_t *h)
 {
 	size_t count;
-	
+
 	count = 0;
 
-	for ( ; h != NULL; h = h->next)
+	while (h)
 	{
 		printf("%d\n", h->n);
 		count++;
+		h = h->next;
 	}
 	return (count);
+
 }
