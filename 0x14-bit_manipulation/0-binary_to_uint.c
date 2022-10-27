f4fc1f4a2b7312c17598aeb74d063a9098d87e04
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-27 15:43:57 -0700

    task 0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * binary_to_uint- Convert binary to unsigned integer
+ * @b: binary to convert
+ *
+ * Return: Always 0
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+
