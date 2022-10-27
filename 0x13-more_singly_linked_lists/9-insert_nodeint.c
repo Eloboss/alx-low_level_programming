9f43640784d7d117f5a84b9af3ddba378cc9c669
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-26 18:18:12 -0700

    task 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -38,7 +38,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	while (i < (idx - 1))
 	{
-		elo = elo->next;
+		*head = (*head)->next;
 		i++;
 	}
 	elo->next = (*head)->next;
