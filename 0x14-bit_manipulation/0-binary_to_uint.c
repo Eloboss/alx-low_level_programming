7f44fb5f651181c0fb2f1edec8db165a4263e10e
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-27 16:18:40 -0700

    task 0

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,4 +9,27 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	unsigned int i;
+	unsigned int sum = 0;
+	int rem;
+	int number;
 
+	for (i = 0; b[i] != NULL; )
+	{
+		i++;
+	}
+
+	if (!b)
+	{
+		return (0);
+	}
+
+	while (b > 0)
+	{
+		rem = number % 10;
+		sum += (rem) * (2 ** i);
+		number = number / 10;
+		i++;
+	}
+	return (sum);
+}
