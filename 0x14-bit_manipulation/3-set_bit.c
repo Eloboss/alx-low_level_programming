cb508b952ef772b9f270ff0569e4227765f34066
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-27 18:11:11 -0700

    task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * set_bit- Set a given bit to 1
+ * @n: Integer to b used
+ * @index: Index
+ *
+ * Return: Always 0
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index > 53)
+	{
+		return (-1);
+	}
+	
+	*n |= (1 << k);
+
+	return (1);
+}
