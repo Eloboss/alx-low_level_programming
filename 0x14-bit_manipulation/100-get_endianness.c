ef69c7520049654255db9699293149552c109bd1
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-27 18:20:25 -0700

    task 6

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * get_endianness- Gets endianness
+ * 
+ * Return: Always 0
+ */
+
+int get_endianness(void)
+{
+		unsigned int b = 1;
+	char *a = (char *)&b;
+
+	if (*a)
+		return (1);
+	else
+		return (0);
+}
