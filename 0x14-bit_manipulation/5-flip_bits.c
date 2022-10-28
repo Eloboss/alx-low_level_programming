202734d27992ce6632fff048b272c47cd1d49c3e
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-27 19:16:32 -0700

    task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -10,4 +10,14 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
+	unsigned long int boss = n ^ m;
+	unsigned int count = 0;
 
+	while (boss)
+	{
+		if (boss & 1ul)
+			count++;
+		boss = boss >> 1;
+	}
+	return (count);
+}
