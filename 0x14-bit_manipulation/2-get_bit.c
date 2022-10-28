30108ba4b3e90cf65f4a381b65c2b372ca8b7c14
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-27 18:02:41 -0700

    task 2

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,6 +11,11 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int answer;
 
+	if (index > 53)
+	{
+		return (-1);
+	}
+
 	answer = ((n >> index) & 1);
 
 	return (answer);
