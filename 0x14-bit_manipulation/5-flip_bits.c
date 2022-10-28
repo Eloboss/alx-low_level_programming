fd52d48d8ddfdecdc3f97d844fe1cef64a730998
Eloboss <elochukwunwaigbo@gmail.com>
2022-10-27 18:49:36 -0700

    task 7

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * flip_bits- Toggles bit from one number to another
+ * @n: Destination string
+ * @m: Source String
+ *
+ * Return: Always 0
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+
