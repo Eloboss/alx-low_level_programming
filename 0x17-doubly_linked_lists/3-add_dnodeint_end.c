eb484e959f029bde1e70a6325a0ae7c4d2c0a03c
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-08 18:34:01 -0800

    task 3

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end- Adds lists at d end
+ * @head: head of d list
+ * @n: position of d node
+ * Return: Always 0
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *elo;
+	dlistint_t *h;
+
+	elo = malloc(sizeof(dlist_int));
+	if (elo == NULL)
+	{
+		return (NULL);
+	}
+	elo->n = n;
+	elo->next = NULL;
+
+	h = *head;
+
+	if (h != NULL)
+	{
+		while (h->next != NULL)
+			h = h->next;
+		h->next = elo;
+	}
+	else
+	{
+		*head = elo;
+	}
+
+	elo->prev = h;
+
+	return (elo);
+}
