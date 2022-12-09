a98c280b7e9c579387b79e2e9537c6493ca57819
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-08 16:46:48 -0800

    task 0

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/*
+ * print_dlistint- print integers
+ * @h: head of d list
+ * Return: Always 0
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	const dlistint_t *elo = h;
+	size_t count;
+	
+	while (elo != NULL)
+	{
+		elo = elo->next;
+		printf("elo->n\n");
+		count++;
+	}
+	return (count);
+}
