2f3e304e101c3875f21488d2ade4c6ac0e035b15
Eloboss <elochukwunwaigbo@gmail.com>
2022-12-12 17:20:42 -0800

    task 3

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/Eloboss/alx-low_level_programming/raw/master/0x18-dynamic_libraries/anonymous.so
-export LD_PRELOAD=/tmp/anonymous
+wget -P /tmp https://github.com/Eloboss/alx-low_level_programming/raw/master/0x18-dynamic_libraries/inject.so
+export LD_PRELOAD=/tmp/inject.so
